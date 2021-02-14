da4ec50f5e0c214cf9fda224aae5975d9f2dee63
bvcbvc <42185819+bvcbvc@users.noreply.github.com>
2021-02-13 21:37:00 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -153,16 +153,7 @@ ui.onLoad(() => {
 	});
 });
 
-ui.addButton("unit-factory", spawning, () => {
-	if (Vars.net.client()) {
-		if (!Vars.player.admin) {
-			Vars.ui.showInfoToast("You egg that would desync", 5);
-			return;
-		}
-	} else if (Vars.state.rules.sector) {
-		Vars.ui.showInfoToast("No cheating! [red]*slaps hand*", 5);
-		return;
-	}
+
 
 	dialog.show();
 }, b => {button = b.get()});
